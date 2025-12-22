42d6634fd0f76f55e6d44a1be923b6f644366855
HannaOdud <annodud@gmail.com>
2025-12-22 23:23:23 +0000

    local starage is done

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+const taskArray = [];
+
 function setup(){
-    
+
+    //get data from local storage
+    const retrivedData = localStorage.getItem("to_do_list");
+    if (retrivedData != null){
+        taskArray = JSON.parse(retrivedData);
+
+    }
+
+    const form = document.getElementById("form");
+    const titleDom = document.getElementById("title");
+    const descriptionDom = document.getElementById("description");
+    const deadlineDom = document.getElementById("deadline");
+    const importantDom = document.getElementById("important");
+    form.addEventListener("submit",(event) =>{
+        event.preventDefault();
+        const title = titleDom.value;
+        const description = descriptionDom.value;
+        const deadline = deadlineDom.value;
+        const important = importantDom.value;
+        const task = {
+            title: title,
+            description: description,
+            deadline: deadline,
+            important: important
+        }
+        taskArray.push(task);
+        ///display task cards
+        localStorage.setItem("to_do_list",  JSON.stringify(taskArray))
+    })
 }
+
+
+
+
+
+
 
 window.onload = setup;
