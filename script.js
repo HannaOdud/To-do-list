dbfc5148bd59c0064a46366e7e58e7853ed9c06b
HannaOdud <annodud@gmail.com>
2025-12-25 12:36:07 +0000

    tasc cards rendered

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const taskArray = [];
+let taskArray = [];
 
 function setup(){
 
@@ -6,7 +6,15 @@ function setup(){
     const retrivedData = localStorage.getItem("to_do_list");
     if (retrivedData != null){
         taskArray = JSON.parse(retrivedData);
-        ///display task cards
+        ///display task cards 
+            ///firstly, we delete existing cards from container line 33/34
+        const container = document.getElementById("container");
+        container.innerHTML = "";
+            ///we drow every card again
+        taskArray.forEach(task =>{
+            taskCard(task);
+        })
+        /////
     }
 
     const form = document.getElementById("form");
@@ -24,12 +32,66 @@ function setup(){
             title: title,
             description: description,
             deadline: deadline,
-            important: important
+            important: important,
+            done: false
         }
         taskArray.push(task);
-        ///display task cards
+        ///display task cards 
+            ///firstly, we delete existing cards from container line 33/34
+        const container = document.getElementById("container");
+        container.innerHTML = "";
+            ///we drow every card again
+        taskArray.forEach(task =>{
+            taskCard(task);
+        })
+        /////
         localStorage.setItem("to_do_list",  JSON.stringify(taskArray))
     })
+}
+function taskCard(task){
+    //read task data
+    const title = task.title;
+    const description = task.description;
+    const deadline = task.deadline;
+    const important = task.important;
+    //create dom elements
+    const div = document.createElement("div");
+    if (important == true){
+        div.classList = "task_card important";
+    }
+    else{ div.classList = "task_card"}
+
+    const h1 = document.createElement("h1");
+    h1.textContent = title;
+    const pDesc = document.createElement("p");
+    pDesc.textContent = description;
+    const pDate = document.createElement("p");
+    pDate.textContent = deadline;
+    //creating Checkbox
+    const pDone = document.createElement("p");
+    pDone.textContent = "Done: ";
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    pDone.appendChild(checkbox);
+    div.appendChild(pDone);
+
+
+    const button = document.createElement("button");
+    button.textContent = "Remove";
+    div.appendChild(h1);
+    div.appendChild(pDesc);
+    div.appendChild(pDate);
+    div.appendChild(button);
+    const container = document.getElementById("container");
+    container.appendChild(div);
+
+
+    button.addEventListener("click",() =>{
+        div.remove();
+    })
+
 }
 
 
